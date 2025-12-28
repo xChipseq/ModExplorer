30676ac784463f6a719c1eaa566e1a024497fd56
Chipseq <108542751+xChipseq@users.noreply.github.com>
2025-12-28 19:32:14 +0100

    config class

---
+++ ModExplorer/ModExplorerConfig.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx.Configuration;
+
+namespace ModExplorer;
+
+public class ModExplorerConfig
+{
+    public ConfigEntry<bool> PrettyCategories { get; init; }
+    public ConfigEntry<bool> PrettySettings { get; init; }
+    
+    public ConfigEntry<int> IntSetting { get; init; }
+    public ConfigEntry<float> FloatSetting { get; init; }
+    
+    public ModExplorerConfig(ConfigFile config)
+    {
+        PrettyCategories = config.Bind("Mod Configs", "Pretty Categories", true,
+            "Makes config categories visually better by adding spaces when there's lack of them.");
+        PrettySettings = config.Bind("Mod Configs", "Pretty Settings", true,
+            "Makes config entries visually better by adding spaces when there's lack of them.");
+
+        IntSetting = config.Bind("Example", "Example Int", 0);
+        FloatSetting = config.Bind("Example", "Example Int", 4.2f);
+    }
+}
