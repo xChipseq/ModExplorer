d879a7da0bd5995ae1a679726cce89ccd8116165
Chipseq <108542751+xChipseq@users.noreply.github.com>
2025-12-28 19:32:28 +0100

    assets

--- ModExplorer/Assets.cs
+++ ModExplorer/Assets.cs
@@ -10,6 +10,13 @@ public static class Assets
     public static BundleAsset<GameObject> MenuPrefab => new(MainBundle, "ModExplorerCanvas"); 
     public static BundleAsset<GameObject> ModListElementPrefab => new(MainBundle, "ModListElement"); 
     
+    public static BundleAsset<GameObject> SettingCategoryPrefab => new(MainBundle, "SettingCategory");
+    public static BundleAsset<GameObject> BoolSettingPrefab => new(MainBundle, "BoolSetting");
+    public static BundleAsset<GameObject> EnumSettingPrefab => new(MainBundle, "EnumSetting");
+    public static BundleAsset<GameObject> FloatSettingPrefab => new(MainBundle, "FloatSetting");
+    public static BundleAsset<GameObject> IntSettingPrefab => new(MainBundle, "IntSetting");
+    public static BundleAsset<GameObject> StringSettingPrefab => new(MainBundle, "StringSetting");
+    
     public static void Load()
     {
         MainBundle = AssetBundleManager.Load("modexplorer");
